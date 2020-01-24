3dc94eb07b5d69f1ebb681b3ede2691f80da1084
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-01-24 15:24:54 +0700

    Create Servo Text Program

---
+++ Test_Servo/Test_Servo.ino
@@ -0,0 +1,137 @@
+#include "ATMega128_Arduino_Pin.h"
+#include "Servo.h"
+#include "CommandHandler.h" 
+#include "Values.h"
+
+#define SERVO_HOME_ANGLE  90
+
+const uint8_t servo_pin[4][3] = {{ PORT_A_0, PORT_A_1, PORT_A_2 }, 
+                                 { PORT_D_0, PORT_D_1, PORT_D_2 },
+                                 { PORT_B_7, PORT_B_6, PORT_B_5 },
+                                 { PORT_F_7, PORT_F_6, PORT_F_5 }};
+
+
+const uint8_t servo_home_angle[4][3] = {{ 90, 90, 90-20 },       //Depan Kanan
+                                        { 90, 90-40, 90+20-40 },       //Belakang Kanan
+                                        { 90, 90, 90-20 },       //Belakang Kiri
+                                        { 90, 90-40, 90+20-40 }};      //Depan Kiri
+
+
+Servo servo[4][3];
+CommandHandler<> SerialCommandHandler(Serial, '[', ']');
+
+
+void setup() {
+    init_command();
+    
+    init_servo();
+    goto_servo_home();
+    delay(5000);
+    
+//    set_leg(0, L_FEMUR, -L_TIBIA+20, 0);
+//    set_leg(1, L_FEMUR, -L_TIBIA, 0);
+//    set_leg(2, L_FEMUR, -L_TIBIA, 0);
+//    set_leg(3, L_FEMUR, -L_TIBIA, 0);
+}
+
+void loop() {
+//    for (byte i = 0; i < 5; i++){
+   
+//    }
+    Serial.println("AAAAAAA");
+    delay(500);
+    SerialCommandHandler.Process();
+}
+
+void init_servo(){
+    for (uint8_t i = 0; i < 4; i++){
+        for (uint8_t j = 0; j < 3; j++){
+            servo[i][j].attach(servo_pin[i][j]);     
+        }
+    } 
+}
+
+void goto_servo_home(){
+    for (uint8_t i = 0; i < 4; i++){
+        for (uint8_t j = 0; j < 3; j++){
+            servo[i][j].write(servo_home_angle[i][j]);
+        }
+    }
+}
+
+void set_leg(byte leg, int x, int y, int z){
+    if (leg < 0 || leg > 3)
+        return;
+
+    int sq_x = x*x;
+    int sq_y = y*y;
+
+    float alpha = acos((SQ_L_FEMUR + sq_x + sq_y - SQ_L_TIBIA) / 
+                       ( 2 * L_FEMUR * sqrt(sq_x + sq_y)));
+    
+    float beta  = acos((SQ_L_FEMUR + SQ_L_TIBIA - (sq_x + sq_y)) / 
+                       (2 * L_FEMUR * L_TIBIA));
+
+    //Radian to Angle
+    alpha = (int)(alpha * 57.29577);
+    beta  = (int)(beta  * 57.29577);
+    
+    servo[leg][1].write(90  + alpha);
+    servo[leg][2].write(90 - beta);    
+}
+
+void trigono_xyz(float x, float y, float z){
+    const float cx = 2; //coxa
+    const float fm = 6.2; //femur
+    const float tb = 8.3; // tibia
+    
+    float L, L1;
+    float alpha, alpha1,alpha2,beta,gama;
+     
+    L1     = sqrt(sq(x) + sq(y));
+    gama   = atan(x / y) / 565.48667764;
+    L      = sqrt(sq(L1 - cx) + sq(z));
+    beta   = acos((sq(tb) + sq(fm) - sq(L)) / (2 * tb * fm))/ 565.48667764;
+    alpha1 = acos(z / L) / 565.48667764;
+    alpha2 = acos((sq(fm) + sq(L) - sq(tb)) / (2 * fm * L)) / 565.48667764;
+    alpha  = alpha1 + alpha2;
+    Serial.println("L1 = " + String(L1));
+    Serial.println("gamma = " + String(gamma));
+    Serial.println("L = " + String(L));
+    Serial.println("beta = " + String(beta));
+    Serial.println("alpha1 = " + String(alpha1));
+    Serial.println("alpha2 = " + String(alpha2));
+    Serial.println("alpha = " + String(alpha));
+    
+}
+
+void init_command(){
+    Serial.begin(9600);
+    SerialCommandHandler.AddCommand(F("servo"), Cmd_SetServoAngle);
+    SerialCommandHandler.SetDefaultHandler(Cmd_Unknown);
+}
+
+
+void Cmd_Unknown()
+{
+  Serial.println(F("Unknown command."));
+}
+
+void Cmd_SetServoAngle(CommandParameter &Parameters){
+    int leg_number = Parameters.NextParameterAsInteger();
+    
+    if (leg_number < 0 || leg_number >= 4)
+        return;
+
+    int servo_number = Parameters.NextParameterAsInteger();
+    
+    if (servo_number < 0 || servo_number >= 3)
+        return;
+      
+    int angle = Parameters.NextParameterAsInteger();
+    
+    if (angle < 0 || angle >= 180)
+        return;
+
+    servo[leg_number][servo_number].write(angle);
+}
