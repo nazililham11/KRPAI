b3894ddafb36466666f76855554d233e9cb821e1
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-02-05 23:15:35 +0700

    Update: Add Ir Sensor

--- Test_Sensor/Test_Sensor.ino
+++ Test_Sensor/Test_Sensor.ino
@@ -2,21 +2,41 @@
 #include "Ultrasonic_lib.h"
 
 Ultrasonic ultrasonic[] = {
-    Ultrasonic(PORT_E_0), //Front
+    Ultrasonic(PORT_E_5), //Front
     Ultrasonic(PORT_C_6), //Left
-    Ultrasonic(PORT_E_2), //Right
+    Ultrasonic(PORT_E_7), //Right
     Ultrasonic(PORT_C_7), //Front Left
-    Ultrasonic(PORT_E_1)  //Front Right
+    Ultrasonic(PORT_E_6)  //Front Right
 };
+
+float ir_distance;
 
 void setup(){
     Serial.begin(115200);
+    Serial.println("Sensor Test");
 }
 
 void loop(){
     read_distance();
+    read_ir_distance();
     print_distance();
-    delay(1000);
+    print_ir_distance();
+    Serial.println();
+    delay(100);
+}
+
+void read_ir_distance()
+{
+    float volts = analogRead(A0) * 0.0048828125;  // value from sensor * (5/1024)
+    ir_distance = 13 * pow(volts, -1);                 // worked out from datasheet graph
+}
+
+void print_ir_distance(void)
+{
+    if (ir_distance <= 30){
+        Serial.print(F("\tIR Distance"));
+        Serial.print(ir_distance);   // print the distance
+    }
 }
 
 void read_distance(){
@@ -28,16 +48,14 @@ void read_distance(){
 }
 
 void print_distance(){
-    Serial.println("Distance");
     Serial.print(F("Front:"));
     Serial.print(String(ultrasonic[US_FRONT].distance));
-    Serial.print("\tLeft:");
+    Serial.print(F("\tLeft:"));
     Serial.print(String(ultrasonic[US_LEFT].distance));
-    Serial.print("\tRight:");
+    Serial.print(F("\tRight:"));
     Serial.print(String(ultrasonic[US_RIGHT].distance));
-    Serial.print("\tFront Left:");
+    Serial.print(F("\tFront Left:"));
     Serial.print(String(ultrasonic[US_FRONT_LEFT].distance));
-    Serial.print("\tFront Right:");
+    Serial.print(F("\tFront Right:"));
     Serial.print(String(ultrasonic[US_FRONT_RIGHT].distance));
-    Serial.println();
 }
